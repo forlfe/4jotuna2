8bdb3161a069c244ff2a10b8bb6d3fb095ad387a
DESKTOP-GQVTP40\haeyeion <haileyhy@naver.com>
2022-08-03 15:15:10 +0900

    송해연 2208031515 커밋

--- miniWorkspace2/miniPrj01/src/shy/Menu.java
+++ miniWorkspace2/miniPrj01/src/shy/Menu.java
@@ -1,6 +1,25 @@
 package shy;
 
+import jhnUtil.InputUtil;
+import main.Main;
+
 public class Menu {
+	
+	
+	public int showRecordMenu() {
+		System.out.println();
+		System.out.println("\n ------- 메인 메뉴 -------");
+		
+		if(Main.loginMember != null) {
+			//로그인 O
+			System.out.println(Main.loginMember.getNick()+" 님 환영합니다.");
+			System.out.println("5. 전체 회원 운동 기록 조회하기");
+			System.out.println("6. 특정 회원 운동 기록 조회하기");
+			
+		}
+		
+		return InputUtil.getInt()
+	}
 
 	public int showPersonalRecordMenu() {
 		System.out.println("");
