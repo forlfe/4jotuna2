d9507e618253b6780cf05cd469e361ed4e1e288a
DESKTOP-GQVTP40\haeyeion <haileyhy@naver.com>
2022-08-04 10:21:06 +0900

    프로젝트 초기셋팅

---
+++ miniWorkspace2/umpaumpaPrj/src/com/umpaumpa/Main.java
@@ -0,0 +1,28 @@
+package com.umpaumpa;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+				Scanner sc = new Scanner(System.in);
+				
+				
+				System.out.println("====== 환영합니다 수영 어플 음파음파입니다 ======");
+				while(true) {
+					System.out.println("1. 로그인");
+					System.out.println("2. 회원가입");
+					int input = sc.nextInt();
+					
+					switch(input) {
+					case 1 : /**/; break;
+					case 2 : /**/ break;
+					case 3 : break;
+					}
+				}
+
+	}
+
+}
